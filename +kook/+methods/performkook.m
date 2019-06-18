ab48784f87c21e0b09125c5dd416f96305b2e926
LeumasD10 <yeshun@shaw.ca>
2019-06-17 23:01:37 -0700

    Removed figures for speed.

--- +kook/+methods/performkook.m
+++ +kook/+methods/performkook.m
@@ -10,23 +10,22 @@ sens_val = 0.75; % the sensitivity (0?1) for the circular Hough transform
 
 %% Canny edge detection
 BWCED = edge(II1_lt,'canny');
-figure();imshow(BWCED);title('Canny edge detection');
+% figure();imshow(BWCED);title('Canny edge detection');
 
 %% Find circles within soot aggregates
 [centersCED, radiiCED, metricCED] = imfindcircles(BWCED,[rmin rmax], 'objectpolarity', 'bright', 'sensitivity', sens_val, 'method', 'twostage');
 % - draw circles
-figure();imshow(OriginalImg,[]);hold;h = viscircles(centersCED, radiiCED, 'EdgeColor','r');
-title('Primary particles overlaid on the original TEM image');
+% figure();imshow(OriginalImg,[]);hold;h = viscircles(centersCED, radiiCED, 'EdgeColor','r');
+% title('Primary particles overlaid on the original TEM image');
 
 
 %% - check the circle finder by overlaying the CHT boundaries on the original image
 R = imfuse(BWCED, OriginalImg,'blend');
-figure();imshow(R,[],'InitialMagnification',500);hold;h = viscircles(centersCED, radiiCED, 'EdgeColor','r');
-title('Primary particles overlaid on the Canny edges and the original TEM image');
+% figure();imshow(R,[],'InitialMagnification',500);hold;h = viscircles(centersCED, radiiCED, 'EdgeColor','r');
+% title('Primary particles overlaid on the Canny edges and the original TEM image');
 dpdist = radiiCED*TEMscale*2;
 
 outputArg1 = dpdist;
 outputArg2 = centersCED;
 outputArg3 = metricCED;
 end
-
